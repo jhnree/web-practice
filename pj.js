b9d1035ef06e7079fe8bc89a3e5ba4d7a779674d
yopijay <53463564+yopijay@users.noreply.github.com>
2019-10-02 13:56:55 +0800

    Create pj.js
    Added ajax format

---
+++ pj.js
@@ -0,0 +1,16 @@
+//$(function() {
+  //$.ajax({
+    //url: 'url mo',
+    //method: 'POST',
+    //data: {
+      //data: input,
+      //data: input,
+    //},
+    //dataType: 'json',
+    //success:function(data) {
+      //console.log(data);
+    //}
+  //});
+//});
+
+AJAX FORMAT para sakin HAHAH
